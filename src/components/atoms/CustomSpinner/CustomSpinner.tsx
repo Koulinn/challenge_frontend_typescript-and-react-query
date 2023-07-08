771e8891a07c05273884a18c8453462c00ba5879
Rafael Lima <rafauxdev@gmail.com>
2023-07-08 07:16:10 +0100

    Create atom CustomSpinner

---
+++ src/components/atoms/CustomSpinner/CustomSpinner.tsx
@@ -0,0 +1,12 @@
+import { Spinner } from '@chakra-ui/react';
+import React from 'react';
+
+function CustomSpinner() {
+    return (
+        <div>
+            <Spinner />
+        </div>
+    );
+}
+
+export default CustomSpinner;
