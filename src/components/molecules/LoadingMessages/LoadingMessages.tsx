d5842b6c914884590b814fba2000e75187e65563
Rafael Lima <rafauxdev@gmail.com>
2023-07-08 07:16:42 +0100

    Create molecule LoadingMessages

---
+++ src/components/molecules/LoadingMessages/LoadingMessages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CustomSpinner from '../../atoms/CustomSpinner/CustomSpinner';
+
+function LoadingMessages() {
+    return (
+        <div>
+            <CustomSpinner />
+        </div>
+    );
+}
+
+export default LoadingMessages;
