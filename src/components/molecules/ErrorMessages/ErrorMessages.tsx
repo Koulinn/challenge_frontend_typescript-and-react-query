cdc1d508104155705ea4e2098cf31965862bac13
Rafael Lima <rafauxdev@gmail.com>
2023-07-08 17:49:13 +0100

    Update ErrorMessages and LoadingMessages

--- src/components/molecules/ErrorMessages/ErrorMessages.tsx
+++ src/components/molecules/ErrorMessages/ErrorMessages.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+// constants
+import { apiMessages } from 'lib/constants';
+
+// We can handle custom error messages and capture errors as well
 function ErrorMessages(props: any) {
-    return <div>{props.code}</div>;
+    return <div>{apiMessages.generalError}</div>;
 }
 
 export default ErrorMessages;
--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,5 +1,3 @@
 export { default as VulnerabilityCard } from './Cards/VulnerabilityCard/VulnerabilityCard';
 
 export { default as ErrorMessages } from './ErrorMessages/ErrorMessages';
-
-export { default as LoadingMessages } from './LoadingMessages/LoadingMessages';
