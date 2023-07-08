37ade32f1c59b6e534febde5acb476f26fa1696a
Rafael Lima <rafauxdev@gmail.com>
2023-07-08 07:15:01 +0100

    Create useVulnerabilities

---
+++ src/hooks/useVulnerabilities.tsx
@@ -0,0 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
+
+import mockData from '../mockData/vulnerabilities.json';
+import VulnerabilitiesType from '../types/VulnerabilitiesTypes';
+
+const mockAPI = (): Promise<VulnerabilitiesType> =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(mockData);
+        }, 700);
+    });
+
+function useVulnerabilities() {
+    return useQuery({
+        queryKey: ['vulnerabilitites'],
+        queryFn: async () => {
+            const data = await mockAPI();
+
+            return data;
+        },
+        onError: (err: any) => {
+            // console.log(err, 'err');
+        },
+    });
+}
+
+export default useVulnerabilities;
