7f967ff8b49213ffcddbb292f018bcfd81184a94
Rafael Lima <rafauxdev@gmail.com>
2023-07-08 17:48:53 +0100

    Update organism index.js

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,6 +1,12 @@
+export { default as CardsViewer } from './CardsViewer/CardsViewer';
+
 export { default as Footer } from './Footer/Footer';
 
+export { default as HandleApiStatus } from './HandleApiStatus/HandleApiStatus';
+
 export { default as Header } from './Header/Header';
+
+export { default as Hero } from './Hero/Hero';
 
 export { default as SearchBar } from './SearchBar/SearchBar';
 
