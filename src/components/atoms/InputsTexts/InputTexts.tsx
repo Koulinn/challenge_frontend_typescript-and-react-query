01d2132618d08cd13c1b23b309ecfd279d4d7fbe
Rafael Lima <rafauxdev@gmail.com>
2023-07-08 16:00:38 +0100

    Prettier InputTexts

--- src/components/atoms/InputsTexts/InputTexts.tsx
+++ src/components/atoms/InputsTexts/InputTexts.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
+//Packages
 import { Input } from '@chakra-ui/react';
+
+// Types
 import { InputTextsTypes } from 'types/InputTypes';
-
-
 
 function InputTexts(props: InputTextsTypes) {
     return <Input {...props} />;
