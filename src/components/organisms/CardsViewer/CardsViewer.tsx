a4261317da3dca01c28e8d4d8e5f87403798dce4
Rafael Lima <rafauxdev@gmail.com>
2023-07-08 17:49:38 +0100

    Add key property to CardsViewer map

--- src/components/organisms/CardsViewer/CardsViewer.tsx
+++ src/components/organisms/CardsViewer/CardsViewer.tsx
@@ -7,7 +7,7 @@ function CardsViewer(props: { cards: VulnerabilitiesType }) {
     return (
         <SimpleGrid minChildWidth="320px" spacing="32px">
             {props.cards.data.map((vulnerability) => (
-                <VulnerabilityCard vulnerability={vulnerability} />
+                <VulnerabilityCard key={vulnerability.uuid} vulnerability={vulnerability} />
             ))}
         </SimpleGrid>
     );
