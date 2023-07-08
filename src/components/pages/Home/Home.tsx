5545fd46f72909c985fa178553b5448a5780d045
Rafael Lima <rafauxdev@gmail.com>
2023-07-08 17:50:03 +0100

    Update Home to display cards

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+
+import { CardsViewer, HandleApiStatus } from 'components/organisms';
+import Hero from 'components/organisms/Hero/Hero';
+import useVulnerabilities from 'hooks/useVulnerabilities';
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    const { data, status } = useVulnerabilities();
+    const notSuccess = status !== 'success';
+
+    if (notSuccess) {
+        return <HandleApiStatus status={status} />;
+    }
+
+    return (
+        <div>
+            <Hero />
+            {status === 'success' && <CardsViewer cards={data} />}
+        </div>
+    );
 }
 
-export default Home
+export default Home;
