531aed0a60143cd0e1f92daf2172ccc339d93e5c
Rafael Lima <rafauxdev@gmail.com>
2023-07-09 11:15:12 +0100

    Export DropdownSelect from index.js

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,5 +1,7 @@
 export { default as CustomSpinner } from './CustomSpinner/CustomSpinner';
 
+export { default as DropdownSelect } from './DropdownSelect/DropdownSelect';
+
 export { default as InputTexts } from './InputsTexts/InputTexts';
 
 export { default as Modal } from './Modal/Modal';
