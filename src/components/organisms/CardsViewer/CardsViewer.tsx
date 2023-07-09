c392f62cc4c502f867c4c1cad4fe292defb60582
Rafael Lima <rafauxdev@gmail.com>
2023-07-09 07:37:39 +0100

    Rework CardsViewer, remove data and pass only the array

--- src/components/organisms/CardsViewer/CardsViewer.tsx
+++ src/components/organisms/CardsViewer/CardsViewer.tsx
@@ -5,19 +5,17 @@ import { SimpleGrid } from '@chakra-ui/react';
 import { VulnerabilityCard } from 'components/molecules';
 
 // Types
-import VulnerabilitiesType, {
-    VulnerabilityDataType,
-} from 'types/VulnerabilitiesTypes';
+import { VulnerabilityDataType } from 'types/VulnerabilitiesTypes';
 
 import VulnerabilityModal from '../VulnerabilityModal/VulnerabilityModal';
 
-function CardsViewer(props: { cards: VulnerabilitiesType }) {
+function CardsViewer(props: { cards: VulnerabilityDataType[] | undefined }) {
     const [vulnerabilityCard, setVulnerabilityCard] =
         useState<VulnerabilityDataType | null>(null);
 
     return (
         <SimpleGrid minChildWidth="320px" spacing="32px">
-            {props.cards.data.map((vulnerability) => (
+            {props.cards?.map((vulnerability) => (
                 <VulnerabilityCard
                     key={vulnerability.uuid}
                     vulnerability={vulnerability}
