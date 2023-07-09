3c688ae10cd6fa09f76bc3ba1b0f8c7e86b813a5
Rafael Lima <rafauxdev@gmail.com>
2023-07-09 11:16:26 +0100

    Update Home to display basic search and categories

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
+// Packages
+import { Box } from '@chakra-ui/react';
+
+// Components
 import { CardsViewer, HandleApiStatus } from 'components/organisms';
 import Hero from 'components/organisms/Hero/Hero';
+import { DropdownSelect, InputTexts, Texts } from 'components/atoms';
+
+// Custom Hooks
 import useVulnerabilities from 'hooks/useVulnerabilities';
+import useSearch from 'hooks/useSearch';
+
+// Constants
+import { dropdownOptions } from 'lib/constants';
 
 function Home() {
     const { data, status } = useVulnerabilities();
+    const { handleSearch, handleSeverity, filteredData } = useSearch(
+        data?.data
+    );
 
     const notSuccess = status !== 'success';
 
@@ -16,7 +30,17 @@ function Home() {
     return (
         <div>
             <Hero />
-            {status === 'success' && <CardsViewer cards={data} />}
+            <Box marginBottom={'16'} maxWidth={'25%'}>
+                <Texts.Label>Search:</Texts.Label>
+                <InputTexts onChange={handleSearch}></InputTexts>
+                <DropdownSelect
+                    variant="flushed"
+                    placeholder="Please select a severity"
+                    onChange={handleSeverity}
+                    options={dropdownOptions.SEVERITY_OPTIONS}
+                />
+            </Box>
+            {status === 'success' && <CardsViewer cards={filteredData} />}
         </div>
     );
 }
