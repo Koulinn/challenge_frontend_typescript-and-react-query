cc00c50af525d954192e6b6919648dbf50bf058f
Rafael Lima <rafauxdev@gmail.com>
2023-07-09 11:15:41 +0100

    Create constant SEVERITY_OPTIONS in dropdownOptions

---
+++ src/lib/constants/dropdownOptions.js
@@ -0,0 +1,26 @@
+const SEVERITY_OPTIONS = [
+    {
+        value: 'low',
+        label: 'Low',
+    },
+    {
+        value: 'medium',
+        label: 'Medium',
+    },
+    {
+        value: 'high',
+        label: 'High',
+    },
+    {
+        value: 'critical',
+        label: 'Critical',
+    },
+    {
+        value: 'info',
+        label: 'Info',
+    },
+];
+
+const dropdownOptions = { SEVERITY_OPTIONS };
+
+export default dropdownOptions;
