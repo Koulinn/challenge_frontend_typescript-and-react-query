e24d23c64372426ab3f70876743d588080895cd2
Rafael Lima <rafauxdev@gmail.com>
2023-07-09 06:30:25 +0100

    Implement VulnerabilityModal component

--- src/components/organisms/CardsViewer/CardsViewer.tsx
+++ src/components/organisms/CardsViewer/CardsViewer.tsx
@@ -1,14 +1,33 @@
+import React, { useState } from 'react';
+
+// Components
 import { SimpleGrid } from '@chakra-ui/react';
 import { VulnerabilityCard } from 'components/molecules';
-import React from 'react';
-import VulnerabilitiesType from 'types/VulnerabilitiesTypes';
+
+// Types
+import VulnerabilitiesType, {
+    VulnerabilityDataType,
+} from 'types/VulnerabilitiesTypes';
+
+import VulnerabilityModal from '../VulnerabilityModal/VulnerabilityModal';
 
 function CardsViewer(props: { cards: VulnerabilitiesType }) {
+    const [vulnerabilityCard, setVulnerabilityCard] =
+        useState<VulnerabilityDataType | null>(null);
+
     return (
         <SimpleGrid minChildWidth="320px" spacing="32px">
             {props.cards.data.map((vulnerability) => (
-                <VulnerabilityCard key={vulnerability.uuid} vulnerability={vulnerability} />
+                <VulnerabilityCard
+                    key={vulnerability.uuid}
+                    vulnerability={vulnerability}
+                    setVulnerabilityCard={setVulnerabilityCard}
+                />
             ))}
+            <VulnerabilityModal
+                vulnerabilityCard={vulnerabilityCard}
+                setVulnerabilityCard={setVulnerabilityCard}
+            />
         </SimpleGrid>
     );
 }
