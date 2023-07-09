9dbb403bc205b3db64ed8c3ee51efdcea6e22778
Rafael Lima <rafauxdev@gmail.com>
2023-07-09 07:37:11 +0100

    Update InputTextsTypes to make them optional

--- src/types/InputTypes.tsx
+++ src/types/InputTypes.tsx
@@ -1,12 +1,12 @@
 // All input interfaces
 
 export interface InputTextsTypes {
-    placeholder: string;
-    size: string;
-    onClick: (e: React.MouseEvent<HTMLInputElement>) => void;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    onblur: (e: React.MouseEventHandler<HTMLInputElement>) => void;
-    variant: string;
-    type: string;
-    isInvalid: boolean;
+    placeholder?: string;
+    size?: string;
+    onClick?: (e: React.MouseEvent<HTMLInputElement>) => void;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onblur?: (e: React.MouseEventHandler<HTMLInputElement>) => void;
+    variant?: string;
+    type?: string;
+    isInvalid?: boolean;
 }
